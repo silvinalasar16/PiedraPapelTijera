f8853d1291e991bcca1a010d27a280eb710bc335
silvinalasar16 <31020685+silvinalasar16@users.noreply.github.com>
2017-09-14 21:00:40 -0300

    arreglo de if
    if

--- juego.js
+++ juego.js
@@ -42,18 +42,18 @@ camino correcto.
 En este paso vamos a crear una función que nos devuelva lo que el usuario elige para guardarlo en
 una variable y usarla después. Los pasos son:
 
-    - Crear una función llamada `obtenerOpcionUsuario` que no recibe parámetros.
-        - Usar un prompt para pedirle al usuario que escriba "piedra", "papel" o "tijera".
-        - Devolver lo que el usuario escribió (Usando la palabra clave `return`).
-    - Asignar en una variable llamada `opcionUsuario` el resultado de llamar a la función
-    `obtenerOpcionUsuario`.
+- Crear una función llamada `obtenerOpcionUsuario` que no recibe parámetros.
+- Usar un prompt para pedirle al usuario que escriba "piedra", "papel" o "tijera".
+- Devolver lo que el usuario escribió (Usando la palabra clave `return`).
+- Asignar en una variable llamada `opcionUsuario` el resultado de llamar a la función
+`obtenerOpcionUsuario`.
 
 */
 function obtenerOpcionUsuario(){
   var objeto=prompt("¿piedra, papel, tijera?");
   return objeto;
 }
- var opcionUsuario=obtenerOpcionUsuario();
+var opcionUsuario=obtenerOpcionUsuario();
 /*
 
 // ==================================================
@@ -64,21 +64,21 @@ function obtenerOpcionUsuario(){
 En este paso vamos a crear una función que nos devuelva lo que "la máquina" elige y guardarlo en
 una variable para usarla después. La opción de "la máquina" la vamos a generar aleatoriamente:
 
-    - Crear una función llamada `obtenerOpcionMaquina` que no recibe parámetros.
-        - Usar el siguiente código para obtener una opción aleatoria.
-            var random = Math.random();
-            var opcionMaquinaRandom = random < 0.33 ? 'piedra' : random < 0.66 ? 'papel' : 'tijera';
-        - Devolver lo que la máquina eligió (Usando la palabra clave `return`).
-    - Asignar en una variable llamada `opcionMaquina` el resultado de llamar a la función
-    `obtenerOpcionMaquina`.
+- Crear una función llamada `obtenerOpcionMaquina` que no recibe parámetros.
+- Usar el siguiente código para obtener una opción aleatoria.
+var random = Math.random();
+var opcionMaquinaRandom = random < 0.33 ? 'piedra' : random < 0.66 ? 'papel' : 'tijera';
+- Devolver lo que la máquina eligió (Usando la palabra clave `return`).
+- Asignar en una variable llamada `opcionMaquina` el resultado de llamar a la función
+`obtenerOpcionMaquina`.
 
 */
 function obtenerOpcionMaquina(){
-var random=Math.random();
-var opcionMaquinaRandom= random <0.33? 'piedra': random <0.66? 'papel': 'tijera';
-return opcionMaquinaRandom;
+  var random=Math.random();
+  var opcionMaquinaRandom= random <0.33? 'piedra': random <0.66? 'papel': 'tijera';
+  return opcionMaquinaRandom;
 }
- var opcionMaquina=obtenerOpcionMaquina();
+var opcionMaquina=obtenerOpcionMaquina();
 
 /*
 
@@ -106,46 +106,89 @@ En este paso vamos a crear una función que reciba lo que el usuario y la máqui
 que nos devuelva una cadena de caracteres con uno de 3 posibles valores: 'victoria', 'empate' o
 'derrota':
 
-    - Crear una función llamada `obtenerResultadoJuego` que reciba 2 parámetros:
-        - La opción del usuario.
-        - La opción de la máquina.
-    - Implementar la lógica correspondiente para deducir si el usuario ganó, empató o perdió:
-        - Para eso vas a tener que implementar varios condicionales combinados. Un ejemplo que
-        te puede ayudar a pensarlo sería algo como:
+- Crear una función llamada `obtenerResultadoJuego` que reciba 2 parámetros:
+- La opción del usuario.
+- La opción de la máquina.
+- Implementar la lógica correspondiente para deducir si el usuario ganó, empató o perdió:
+- Para eso vas a tener que implementar varios condicionales combinados. Un ejemplo que
+te puede ayudar a pensarlo sería algo como:
 
-            if (el usuario elegió piedra) {
-                if (la máquina eligió piedra) {
-                    empate
-                }
+if (el usuario elegió piedra) {
+if (la máquina eligió piedra) {
+empate
+}
 
-                if (la máquina eligió papel) {
-                    perdio
-                }
+if (la máquina eligió papel) {
+perdio
+}
 
-                if (la máquina eligió tijera) {
-                    victoria
-                }
-            }
+if (la máquina eligió tijera) {
+victoria
+}
+}
 
-    - Asignar en una variable llamada `resultadoJuego` el resultado de llamar a la función
-    `obtenerResultadoJuego` (No te olvides los parámetros).
+- Asignar en una variable llamada `resultadoJuego` el resultado de llamar a la función
+`obtenerResultadoJuego` (No te olvides los parámetros).
 
 */
 
-function obtenerResultadoJuego(opcionDelUsuario, opcionDeLaMaquina){
-  var resultado;
-  if (opcionDelUsuario='piedra'){
-    if(opcionDeLaMaquina='piedra'){
-      resultado='Empate';
-        }
-      }
+function obtenerResultadoJuego(opcionUsuario, opcionMaquina){
+
+  if (opcionUsuario=='piedra'){
+    if(opcionMaquina=='piedra'){
+        return 'Empate';
+    }
+  }
+
+  if (opcionUsuario=='papel'){
+    if(opcionMaquina=='papel'){
+        return 'Empate';
+    }
+  }
+  if (opcionUsuario=='tijera'){
+    if(opcionMaquina=='tijera'){
+      return 'Empate';
+    }
+  }
+
+  if (opcionUsuario=='piedra'){
+    if (opcionMaquina=='papel'){
+      return 'Perdiste';
+    }
+  }
+
+  if (opcionUsuario=='papel'){
+    if (opcionMaquina=='tijera'){
+      return 'Perdiste';
+    }
+  }
+
+  if (opcionUsuario=='tijera'){
+    if (opcionMaquina=='piedra'){
+      return 'Perdiste';
+    }
+  }
+
+  if (opcionUsuario=='papel'){
+    if (opcionMaquina=='piedra'){
+      return 'GANASTE';
+    }
+  }
+
+  if (opcionUsuario=='tijera'){
+    if (opcionMaquina=='papel'){
+    return 'GANASTE';
+    }
+  }
+
+  if (opcionUsuario=='piedra'){
+    if (opcionMaquina=='tijera'){
+      return 'GANASTE';
+    }
 }
-
-
-console.log(resultado);
-
-
-
+}
+var resultadoJuego=obtenerResultadoJuego(opcionUsuario, opcionMaquina);
+console.log(resultadoJuego);
 /*
 
 // ==================================================
@@ -173,15 +216,15 @@ En este paso vamos a usar la variable `resultadoJuego` para mostrar el mensaje c
 usuario en el sitio. Para ello, vamos a crear una función que reciba el resultado del juego y pueda
 mostrar el mensaje correspondiente al usuario:
 
-    - Crear una función llamada `mostrarResultado` que reciba como parámetro el resultado del juego.
-    - En base al resultado indicado por el parámetro:
-        - Buscar y guardar en una variable el nodo de HTML que represente el resultado del juego
-        (`#victoria`, `#empate` o `#derrota`) usando `getElementById`.
-        - Clonar y guardar en una variable el nodo de HTML del punto anterior.
-        - Buscar y guardar en una variable el nodo de HTML con id `resultado-juego`.
-        - Buscar y guardar en una variable el padre del elemento con id `resultado-juego`.
-        - Reemplazar en el padre (por medio de la función `replaceChild`) el nodo de HTML con id
-        `resultado-juego` por el que tiene la información del resultado del juego.
-    - Llamar a la función `mostrarResultado` pasandole como parámetro el resultado del juego.
+- Crear una función llamada `mostrarResultado` que reciba como parámetro el resultado del juego.
+- En base al resultado indicado por el parámetro:
+- Buscar y guardar en una variable el nodo de HTML que represente el resultado del juego
+(`#victoria`, `#empate` o `#derrota`) usando `getElementById`.
+- Clonar y guardar en una variable el nodo de HTML del punto anterior.
+- Buscar y guardar en una variable el nodo de HTML con id `resultado-juego`.
+- Buscar y guardar en una variable el padre del elemento con id `resultado-juego`.
+- Reemplazar en el padre (por medio de la función `replaceChild`) el nodo de HTML con id
+`resultado-juego` por el que tiene la información del resultado del juego.
+- Llamar a la función `mostrarResultado` pasandole como parámetro el resultado del juego.
 
 */
